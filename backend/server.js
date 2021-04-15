c696d59aedbae83327e3cf71f6d97899a51293eb
Mehmet Eray Sürmeli <surmelimehmeteray@gmail.com>
2021-04-16 00:05:46 +0300

    firstday

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+app.listen(PORT, () =>
+    console.log(`------- Server is running on ${PORT} -------`)
+);
