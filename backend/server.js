a0bffd50d42e1ba91f25371074608195ea8c1da0
Mehmet Eray Sürmeli <surmelimehmeteray@gmail.com>
2021-04-16 16:41:57 +0300

    database post req added

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json()); //req.body
+app.use("/auth", require("./routes/jwtauth"));
 
+//app.use("/dashboard", require("./routes/dashboard"));
 app.listen(PORT, () =>
     console.log(`------- Server is running on ${PORT} -------`)
 );
